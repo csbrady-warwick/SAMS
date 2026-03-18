bae60f2aa8eafda538f35d1605362d30d68dccab
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-03-18 17:30:01 +0000

    NOT WORKING but it compiles here

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@
 
 #include "LARE3DSingleTemperature/shared_data.h"
 #include "LARE3DNeutralFluid/shared_data.h"
+#include "TWOFLUID/twofluid.h"
 
 #include "builtInBoundaryConditions.h"
 
@@ -40,7 +41,7 @@ int main(int argc, char *argv[]){
     SAMS::printWelcomeMessage();
 
     //Create and initialize the runner
-    SAMS::runner<LARE::LARE3D<>, LARE::LARE3DST<>, LARE::LARE3DNF<>, LARE::LARE3DInitialConditions<>, examples::SodShockTube<>, examples::BrioAndWu<>, 
+    SAMS::runner<LARE::LARE3D<>, LARE::LARE3DST<>, LARE::LARE3DNF<>, TWOFLUID::PIP<>, LARE::LARE3DInitialConditions<>, examples::SodShockTube<>, examples::BrioAndWu<>, 
 		    examples::MHDRotor<>, examples::OrszagTang<>, examples::OrszagTang3D<>, examples::EmeryWindTunnel<>, 
         examples::KarmanVortex<>> runner;
     runner.initialize(argc, argv);
